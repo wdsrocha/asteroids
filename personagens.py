462e83c570ac3813f0a6987d422a7d18b9c4a1c6
tiagoweb <tiagoweb@gmail.com>
2017-06-09 02:00:11 -0400

    asteroides

--- personagens.py
+++ personagens.py
@@ -25,9 +25,30 @@ def cria_nave(posicao):
     tela.blit(nave, posicao)
 
 
+# Personagem: Asteroide
+def cria_arteroide(posicao):
+    arteroide = pygame.surface.Surface((90, 82))
+
+    pontos_asteroide_1 = (
+        (19, 0), (40, 18), (60, 0), (80, 18), (64, 38), (78, 59), (50, 80), (15, 80), (0, 63), (0, 20))
+
+    # sendo criado
+    pontos_asteroide_2 = (
+        (19, 0), (40, 18), (60, 0), (80, 18), (64, 38), (78, 59), (50, 80), (15, 80), (0, 63), (0, 20))
+
+    # sendo criado
+    pontos_asteroide_3 = (
+        (19, 0), (40, 18), (60, 0), (80, 18), (64, 38), (78, 59), (50, 80), (15, 80), (0, 63), (0, 20))
+
+    asteroides = [pontos_asteroide_1, pontos_asteroide_2, pontos_asteroide_3]
+
+    pygame.draw.polygon(arteroide, WHITE, asteroides[random.randint(0, 2)], 1)
+
+    tela.blit(arteroide, posicao)
 
 
 cria_nave((200, 200))
+cria_arteroide((300, 300))
 
 pygame.display.update()
 
