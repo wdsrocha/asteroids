eb1084c3424f9e67e491c0c7ec3e8c8990ac23cf
tiagoweb <tiagoweb@gmail.com>
2017-06-09 01:11:13 -0400

    reorganização dos assets

--- tela.py
+++ tela.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame import *
 
-black = (0,0,0)
+BLACK = (0,0,0)
 
 pygame.init()
 
@@ -9,8 +9,20 @@ tela = (800, 600)
 
 tela = pygame.display.set_mode(tela)
 
-pygame.display.set_caption("Hello World")
+pygame.display.set_caption("Asteroids")
 
-tela.fill(black)
+tela.fill(BLACK)
+
+pygame.display.update()
+
+finaliza = False
+
+while not finaliza:
+
+    # --- Loop principal
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            finaliza = True
 
 pygame.quit()
