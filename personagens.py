a29eb988cdc9d37dbf9bbd9bd153c74357d55ec7
tiagoweb <tiagoweb@gmail.com>
2017-06-09 06:48:30 -0400

    asteroides

--- personagens.py
+++ personagens.py
@@ -24,10 +24,11 @@ def cria_nave(posicao):
     nave_linha3 = pygame.draw.line(nave, WHITE, (3, 24), (21, 24), 1)
     tela.blit(nave, posicao)
 
+
 # Personagem: Asteroide
 # Função que cria asteroides de formatos diferentes aleatoriamente
 def cria_arteroide(posicao):
-    arteroide = pygame.surface.Surface((90, 82))
+    arteroide = pygame.surface.Surface((90, 84))
 
     pontos_asteroide_1 = (
         (19, 0), (40, 18), (60, 0), (80, 18), (64, 38), (78, 59), (50, 80), (15, 80), (0, 63), (0, 20))
@@ -36,9 +37,8 @@ def cria_arteroide(posicao):
         (3, 21), (33, 21), (22, 3), (52, 3), (82, 21), (82, 31), (46, 42), (82, 62), (63, 82), (47, 71), (19, 82),
         (3, 51))
 
-    # sendo criado
     pontos_asteroide_3 = (
-        (19, 0), (40, 18), (60, 0), (80, 18), (64, 38), (78, 59), (50, 80), (15, 80), (0, 63), (0, 20))
+        (32, 5), (63, 5), (82, 33), (82, 47), (53, 82), (32, 82), (38, 47), (18, 82), (3, 53), (21, 43), (3, 35))
 
     asteroides = [pontos_asteroide_1, pontos_asteroide_2, pontos_asteroide_3]
 
