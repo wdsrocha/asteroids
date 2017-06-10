f1bdb01c19a588c0953a93490cd3046711899929
adhamlucas <adhamlucas20@gmail.com>
2017-06-10 17:01:05 -0400

    frame(70)

---
+++ testes/teste.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480), 0, 32)
+
+pygame.display.set_caption('Hello World')
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+    screen.blit(pygame.Surface(screen.get_size()), (0, 0))
+    pygame.display.update()
+
+    time_passed = clock.tick(30)
