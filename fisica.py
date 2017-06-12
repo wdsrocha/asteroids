3901e539e328a46e041c17a14e36389de7b3935d
Wesley Rocha <wesleysr1997@gmail.com>
2017-06-12 06:50:59 -0400

    Organizando e adicionando criação de vetor unitário baseado em angulo

--- fisica.py
+++ fisica.py
@@ -1,8 +1,25 @@
 from pygame.math import Vector2 as Vector
 
 VELOCIDADE_MAXIMA = 3
-LARGURA = 800
-ALTURA = 600
+
+#Retorna um dicionário com as características de um corpo físico
+def cria_corpo(x, y):
+    corpo = {}
+    corpo['velocidade'] = Vector(0, 0)
+    corpo['aceleracao'] = Vector(0, 0)
+    corpo['posicao'] = Vector(x, y)
+    corpo['massa'] = 1
+    corpo['direcao'] = 0
+    return corpo
+
+
+def atualiza_corpo(corpo):
+    corpo['velocidade'] += corpo['aceleracao']
+    corpo['aceleracao'] = Vector(0, 0)
+    corpo['velocidade'] = limita_vetor(corpo['velocidade'], VELOCIDADE_MAXIMA)
+    corpo['posicao'] += corpo['velocidade']
+    return corpo
+
 
 def aplica_atrito(corpo, coeficiente_de_atrito):
     if corpo['velocidade'] == Vector(0, 0):
@@ -22,105 +39,5 @@ def limita_vetor(u, lim):
     return u
 
 
-def atualiza(corpo):
-    corpo['velocidade'] += corpo['aceleracao']
-    corpo['velocidade'] = limita_vetor(corpo['velocidade'], VELOCIDADE_MAXIMA)
-
-    if corpo['posicao'][0] > LARGURA:
-        corpo['posicao'][0] = 0
-    if corpo['posicao'][0] < 0:
-        corpo['posicao'][0] = LARGURA
-
-    if corpo['posicao'][1] > ALTURA:
-        corpo['posicao'][1] = 0
-    if corpo['posicao'][1] < 0:
-        corpo['posicao'][1] = ALTURA
-
-    corpo['posicao'] += corpo['velocidade']
-
-    corpo['aceleracao'] = Vector(0, 0)
-    return corpo
-
-
-def cria_corpo():
-    '''Retorna um dicionário com as características de um corpo físico'''
-    corpo = {}
-    corpo['velocidade'] = Vector(0, 0)
-    corpo['aceleracao'] = Vector(0, 0)
-    corpo['posicao'] = Vector(LARGURA / 2, ALTURA / 2)
-    corpo['massa'] = 1
-    corpo['angulo'] = 0
-    return corpo
-
-
-if __name__ == '__main__':
-    import pygame, fisica, math, personagens, random
-
-    pygame.init()
-    tela = pygame.display.set_mode((LARGURA, ALTURA))
-    clock = pygame.time.Clock()
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    ROTACAO = 0
-    # nave_surface = pygame.image.load('./assets/images/jogador.png')
-
-    nave_surface = personagens.cria_nave(tela, (400, 200))
-
-    nave_surface = pygame.transform.rotozoom(nave_surface, -90, 1)
-
-    nave = fisica.cria_corpo()
-
-    nave_pos = Vector(200, 150)
-    nave_speed = 1000
-    nave_rotation = 90
-    nave_rotation_speed = 10000  # Graus por segundo
-
-    while 1:
-
-        background = pygame.image.load('./assets/images/space.jpg').convert()
-        tela.blit(background, (0, 0))
-
-        clock.tick(60)
-
-        forca = Vector(0, 0)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-
-        keys = pygame.key.get_pressed()
-
-        rotation_direction = 0.
-
-        if keys[pygame.K_LEFT]:
-            rotation_direction = +1.0
-
-        if keys[pygame.K_RIGHT]:
-            rotation_direction = -1.0
-
-        if keys[pygame.K_LSHIFT]:
-            nave['posicao'] = random.randint(0,LARGURA), random.randint(0, ALTURA)
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                pygame.draw.polygon(nave_surface, WHITE, ((13, 17), (0, 13), (13, 9)), 1)
-                teta = math.radians(nave_rotation)
-                x = math.cos(teta)
-                y = math.sin(teta)
-                forca += Vector(x, -y)
-        else:
-            pygame.draw.polygon(nave_surface, BLACK, ((13, 17), (0, 13), (13, 9)), 1)
-
-        rotated_nave = pygame.transform.rotate(nave_surface, nave_rotation)
-
-        time_based = clock.tick()
-        time_passed_seconds = time_based / 1000.0
-
-        nave_rotation += rotation_direction * nave_rotation_speed * time_passed_seconds
-
-        nave = aplica_forca(nave, forca)
-        nave = atualiza(nave)
-        nave = aplica_atrito(nave, 0.15)
-        tela.blit(rotated_nave, nave['posicao'])
-        pygame.display.update()
+def cria_vetor_unitario(angulo):
+    return Vector(math.cos(angulo), -math.sin(angulo))
