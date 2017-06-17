dcc303b2968f5fb84ef74ddda97b8a8178ab4cdf
Vitor Simões <vitor6427126@gmail.com>
2017-06-16 20:19:10 -0400

    atualização da rotação.

--- nave.py
+++ nave.py
@@ -1,3 +1,7 @@
+import pygame
+from asteroids.vector2 import Vector2
+
+
 # Aplica força no corpo da nave, alterando sua aceleração
 def ativa_propulsao(nave):
     angulo = math.radians(nave['corpo']['direcao'])
@@ -5,10 +9,22 @@ def ativa_propulsao(nave):
     nave['corpo'] = fisica.aplica_forca(nave['corpo'], forca)
     return nave
 
+
 # Tudo daqui pra baixo falta arrumar
 def atualiza_nave(nave):
-   nave['corpo'] = fisica.atualiza_corpo(nave['corpo']) 
-    
+    nave['corpo'] = fisica.atualiza_corpo(nave['corpo'])
+
 
 def desenha_nave(nave):
     pass
+
+
+# def rotaciona_nave(sprite_pos,w,h,rotation_direction,tempo_passado_segundos):
+#     sprite_rotation = 0
+#     sprite_rotation_speed = 360
+#     rotation_direction = 0.
+#
+#     sprite_rotation += rotation_direction * sprite_rotation_speed * tempo_passado_segundos
+#     sprite_draw_pos = Vector2(sprite_pos.x - w / 2, sprite_pos.y - h / 2)
+#
+#     return (sprite_draw_pos.x, sprite_draw_pos.y)
--- rotacao.py
+++ rotacao.py
@@ -1,6 +1,6 @@
 from sys import exit
-from untitled.gameobjeto.vector2 import Vector2
-# from pygame.math import Vector2
+from asteroids.vector2 import Vector2
+from asteroids import nave
 
 if __name__ == '__main__':
     import pygame, math, random
@@ -26,20 +26,21 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
 
     sprite_pos = Vector2(200, 150)
-    sprite_speed = 300
     sprite_rotation = 0
     sprite_rotation_speed = 360  # Graus por segundo
-    heading = Vector2()
     # loop do game
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+        # limitador para nao passar de 30 interacoes por segundo
+        tempo_passado = clock.tick()
+        tempo_passado_segundos = tempo_passado / 1000.0
+
         # retornar uma tecla pressionada
         teclas_precionadas = pygame.key.get_pressed()
 
         rotation_direction = 0.
-        movement_direction = 0.
 
         if teclas_precionadas[pygame.K_LEFT]:
             rotation_direction = +1.0
@@ -47,38 +48,13 @@ if __name__ == '__main__':
         if teclas_precionadas[pygame.K_RIGHT]:
             rotation_direction = -1.0
 
-        if teclas_precionadas[pygame.K_LSHIFT]:
-            nave_posicao = random.randint(0, 800), random.randint(0, 600)
-
-        if teclas_precionadas[pygame.K_UP]:
-            rotation_direction = +1.0
-
-        if teclas_precionadas[pygame.KEYDOWN]:
-            rotation_direction = -1.0
-
-        # coloca uma surperficie em cima da tela.
-        tela.blit(plano_fundo, (0, 0))
-
         rotated_sprite = pygame.transform.rotate(sprite, sprite_rotation)
         w, h = rotated_sprite.get_size()
         sprite_draw_pos = Vector2(sprite_pos.x - w / 2, sprite_pos.y - h / 2)
-        tela.blit(rotated_sprite, (sprite_draw_pos.x, sprite_draw_pos.y))
-
-        # limitador para nao passar de 30 interacoes por segundo
-        tempo_passado = clock.tick()
-        tempo_passado_segundos = tempo_passado / 1000.0
-
         sprite_rotation += rotation_direction * sprite_rotation_speed * tempo_passado_segundos
-        heading_x = math.sin(sprite_rotation * math.pi / 180.0)
-        heading_y = math.cos(sprite_rotation * math.pi / 180.0)
-        heading = Vector2(heading_x, heading.y)
-        heading *= movement_direction
-        # sprite_pos *= heading * sprite_speed * tempo_passado_segundos
 
-        # if posicao > 800:
-        #     posicao -= 800
-        # if posicao > 600:
-        #     posicao -= 600
-
+        # coloca uma surperficie em cima da tela.
+        tela.blit(plano_fundo, (0, 0))
+        tela.blit(rotated_sprite, (sprite_draw_pos.x, sprite_draw_pos.y))
         # atualiza a telaR
         pygame.display.update()
