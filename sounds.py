fa4b1f6ad12cc2fb834345ef084baabe71c96576
tiagoweb <tiagoweb@gmail.com>
2017-06-19 17:14:48 -0400

    sounds - explosões dos asteroides

--- sounds.py
+++ sounds.py
@@ -7,19 +7,37 @@ def tiro_nave():
     pygame.mixer.music.load('assets/sounds/fire.wav')
     pygame.mixer.music.play()
 
-
 def turbina_nave():
-    pygame.mixer.music.load('assets/sounds/thrust.wav')
+    pygame.mixer.music.load('assets/sounds/beat1.wav')
     pygame.mixer.music.play()
 
+def tiro_patrulha_grande():
+    pygame.mixer.music.load('assets/sounds/saucerBig.wav')
+    pygame.mixer.music.play()
+
+def tiro_patrulha_pequena():
+    pygame.mixer.music.load('assets/sounds/saucerSmall.wav')
+    pygame.mixer.music.play()
+
+def explosao_asteroide_grande():
+    pygame.mixer.music.load('assets/sounds/bangLarge.wav')
+    pygame.mixer.music.play()
+
+def explosao_asteroide_medio():
+    pygame.mixer.music.load('assets/sounds/bangmedium.wav')
+    pygame.mixer.music.play()
+
+def explosao_asteroide_pequeno():
+    pygame.mixer.music.load('assets/sounds/bangSmall.wav')
+    pygame.mixer.music.play()
 
 def tiro_patrulha_grande():
     pygame.mixer.music.load('assets/sounds/saucerBig.wav')
     pygame.mixer.music.play()
 
 
-def tiro_patrulha_pequena():
-    pygame.mixer.music.load('assets/sounds/saucerSmall.wav')
+def tiro_patrulha_grande():
+    pygame.mixer.music.load('assets/sounds/saucerBig.wav')
     pygame.mixer.music.play()
 
 
