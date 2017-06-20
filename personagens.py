ea94e8aeeb626b8d5423be7d2ccaf9dc378bc737
tiagoweb <tiagoweb@gmail.com>
2017-06-20 00:11:29 -0400

    transparência da nave

--- personagens.py
+++ personagens.py
@@ -6,7 +6,7 @@ BLACK = (0, 0, 0)
 
 # Personagem: Nave Espacial
 def cria_nave(tela, posicao):
-    nave = pygame.surface.Surface((24, 38))
+    nave = pygame.surface.Surface((24, 38), pygame.SRCALPHA, 32).convert_alpha()
     nave_linha1 = pygame.draw.line(nave, WHITE, (1, 34), (12, 1), 1)
     nave_linha2 = pygame.draw.line(nave, WHITE, (24, 34), (12, 1), 1)
     nave_linha3 = pygame.draw.line(nave, WHITE, (3, 24), (21, 24), 1)
@@ -16,7 +16,7 @@ def cria_nave(tela, posicao):
 
 
 def turbina(tela, nave):
-    turbina_nave = pygame.surface.Surface((24, 38))
+    turbina_nave = pygame.surface.Surface((24, 38), pygame.SRCALPHA, 32).convert_alpha()
     pygame.draw.polygon(turbina_nave, WHITE, ((8, 24), (12, 38), (16, 24)), 1)
     tela.blit(turbina_nave, (0, 0))
     return turbina_nave
@@ -31,7 +31,7 @@ def missil(tela):
 # Personagem: Asteroide
 # Função que cria asteroides de formatos diferentes aleatoriamente
 def cria_arteroide(tela, posicao):
-    arteroide = pygame.surface.Surface((90, 84))
+    arteroide = pygame.surface.Surface((90, 84), pygame.SRCALPHA, 32).convert_alpha()
 
     pontos_asteroide_1 = (
         (19, 0), (40, 18), (60, 0), (80, 18), (64, 38), (78, 59), (50, 80), (15, 80), (0, 63), (0, 20))
@@ -53,7 +53,7 @@ def cria_arteroide(tela, posicao):
 # função que cria o personagem patrulha
 
 def cria_patrulha(tela, posicao, tamanho):
-    patrulha = pygame.surface.Surface((45, 27))
+    patrulha = pygame.surface.Surface((45, 27), pygame.SRCALPHA, 32).convert_alpha()
 
     contornos = (
         (16, 0), (28, 0), (33, 9), (43, 17), (32, 26), (12, 26), (1, 17), (11, 9))
