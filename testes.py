c764806e6047202eb825b02edee6ce9aea61170d
Wesley Rocha <wesleysr1997@gmail.com>
2017-06-21 12:39:07 -0400

    Tentativa de adicionar redimensionamento

--- testes.py
+++ testes.py
@@ -89,7 +89,7 @@ if __name__ == '__main__':
                         candidatos.append((i, j))
             random.shuffle(candidatos)
             for i in range(QUANTIDADE_ASTEROIDES):
-                asteroides.append(asteroide.cria_asteroide(candidatos[i], 0))
+                asteroides.append(asteroide.cria_asteroide(candidatos[i], 2))
 
         for asteroide_atual in asteroides:
             asteroide.atualiza_asteroide(asteroide_atual)
